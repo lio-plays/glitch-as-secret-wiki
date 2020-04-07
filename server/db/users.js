ab95002463516e2292ca3a66b1aafb3dcdf8d8b8
Glitch (familiar-caramel-organ) <none>
2020-04-07 15:17:40 +0000

    uses password from .env

--- server/db/users.js
+++ server/db/users.js
@@ -1,6 +1,5 @@
 var records = [
-    { id: 1, username: 'jack', password: 'secret', displayName: 'Jack', emails: [ { value: 'jack@example.com' } ] }
-  , { id: 2, username: 'jill', password: 'birthday', displayName: 'Jill', emails: [ { value: 'jill@example.com' } ] }
+    { id: 1, username: 'owner', password: process.env.SECRET, displayName: 'Owner', emails: [ { value: 'owner@example.com' } ] }
 ];
 
 exports.findById = function(id, cb) {
