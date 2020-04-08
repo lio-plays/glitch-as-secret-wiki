82fa92d7c9a592bf458e6dbcfd942700ddec1c82
Glitch (familiar-caramel-organ) <none>
2020-04-08 18:30:33 +0000

    serves with passport and login, but does not check

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var passport = require('passport');
-var Strategy = require('passport-local').Strategy;
-var db = require('./db');
+var express = require("express");
+var passport = require("passport");
+var Strategy = require("passport-local").Strategy;
+var db = require("./db");
 
+const fsp = require("fs").promises;
 
 // Configure the local strategy for use by Passport.
 //
@@ -10,16 +11,22 @@ var db = require('./db');
 // (`username` and `password`) submitted by the user.  The function must verify
 // that the password is correct and then invoke `cb` with a user object, which
 // will be set at `req.user` in route handlers after authentication.
-passport.use(new Strategy(
-  function(username, password, cb) {
+passport.use(
+  new Strategy(function(username, password, cb) {
     db.users.findByUsername(username, function(err, user) {
-      if (err) { return cb(err); }
-      if (!user) { return cb(null, false); }
-      if (user.password != password) { return cb(null, false); }
+      if (err) {
+        return cb(err);
+      }
+      if (!user) {
+        return cb(null, false);
+      }
+      if (user.password != password) {
+        return cb(null, false);
+      }
       return cb(null, user);
     });
-  }));
-
+  })
+);
 
 // Configure Passport authenticated session persistence.
 //
@@ -33,27 +40,32 @@ passport.serializeUser(function(user, cb) {
 });
 
 passport.deserializeUser(function(id, cb) {
-  db.users.findById(id, function (err, user) {
-    if (err) { return cb(err); }
+  db.users.findById(id, function(err, user) {
+    if (err) {
+      return cb(err);
+    }
     cb(null, user);
   });
 });
-
-
-
 
 // Create a new Express application.
 var app = express();
 
 // Configure view engine to render EJS templates.
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 
 // Use application-level middleware for common functionality, including
 // logging, parsing, and session handling.
-app.use(require('morgan')('combined'));
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+app.use(require("morgan")("combined"));
+app.use(require("body-parser").urlencoded({ extended: true }));
+app.use(
+  require("express-session")({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
 // Initialize Passport and restore authentication state, if any, from the
 // session.
@@ -61,32 +73,58 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Define routes.
-app.get('/',
-  function(req, res) {
-    res.render('home', { user: req.user });
-  });
+app.get("/login", function(req, res) {
+  res.render("login");
+});
 
-app.get('/login',
-  function(req, res){
-    res.render('login');
-  });
-  
-app.post('/login', 
-  passport.authenticate('local', { failureRedirect: '/login' }),
+app.post(
+  "/login",
+  passport.authenticate("local", { failureRedirect: "/login" }),
   function(req, res) {
-    res.redirect('/');
-  });
-  
-app.get('/logout',
-  function(req, res){
-    req.logout();
-    res.redirect('/');
-  });
+    res.redirect("/");
+  }
+);
 
-app.get('/profile',
-  require('connect-ensure-login').ensureLoggedIn(),
-  function(req, res){
-    res.render('profile', { user: req.user });
-  });
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+app.get("/profile", require("connect-ensure-login").ensureLoggedIn(), function(
+  req,
+  res
+) {
+  res.render("profile", { user: req.user });
+});
+
+async function html(req, res, subpath) {
+  const path = __dirname + "/.." + subpath;
+  const content1 = await fsp.readFile(path, "utf8");
+  const content = "" + content1.match(/\<body\>(.*)\<\/body\>/s)[1];
+  res.render("home", { user: req.user, content: content });
+}
+
+async function raw(req, res, type) {
+  const path = __dirname + "/.." + req.path;
+  const content = await fsp.readFile(path, "utf8");
+  res.type(type);
+  res.send(content);
+}
+
+app.get("/", function(req, res) {
+  html(req, res, "/index.html");
+});
+
+app.get(/.*\.html$/, function(req, res) {
+  html(req, res, req.path);
+});
+
+app.get(/.*\.js$/, function(req, res) {
+  raw(req, res, ".js");
+});
+
+app.get(/.*\.md$/, function(req, res) {
+  raw(req, res, ".txt");
+});
 
 app.listen(3000);
